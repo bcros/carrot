7f49077955aaf9ea3a134bd8f5925c343e096f34
Joe Cross <joe.mcross@gmail.com>
2013-01-29 23:08:42 -0800

    Some debugging script...
    Found this floating around.
    
    Not sure if it's useful at all, but whatever.

---
+++ debugging/__init__.py
@@ -0,0 +1,16 @@
+## carrot.debugging.__init__
+
+
+def class_name(obj):
+    """
+    Returns the class name of an object.
+    Includes namespaces; for example-
+    in module a.py:
+    class b(object):
+        def __init__(self):
+            class c(object): pass
+        self.c = c()
+    d = b().c
+    print class_name(d) # prints "a.b.c"
+    """
+    return ".".join(str(obj.__class__).split("'")[1].split(".")[1:])
